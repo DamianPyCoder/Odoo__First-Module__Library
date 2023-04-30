9a43196440483edec46667873ca6fa046d3a5446
Damian R. Molin <119323521+DamianPyCoder@users.noreply.github.com>
2023-05-01 00:03:19 +0200

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,1 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library",
 
+    'summary': """
+        Books,ISBN,Author,Library""",
+
+    'description': """
+        Aplicació per emmagatzemar llibres
+    """,
+
+    'author': "Damian M Rodriguez",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Books',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
